0807b2fc2b048538c7834c85122eca6736dad6a6
Ruan Pearce-Authers <ruanpa@outlook.com>
2016-01-31 14:53:09 +0000

    Deactivate player cameras on remotes

---
+++ Assets/Game/Player.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class Player : MonoBehaviour
+{
+	private NetworkIdentity m_ident;
+	private Camera m_camera;
+
+	void Awake()
+	{
+		m_ident = GetComponent<NetworkIdentity>();
+		m_camera = GetComponentInChildren<Camera>();
+	}
+
+	void Start()
+	{
+		m_camera.gameObject.SetActive(m_ident.isLocalPlayer);
+	}
+}
